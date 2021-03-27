f68be1bcb393ac608ce3e36325d9df939cf43230
umerkang66 <umerkang66@gmail.com>
2021-03-27 18:53:37 +0500

    Removed console.log

--- src/js/controller.js
+++ src/js/controller.js
@@ -124,7 +124,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-
-  console.log('consoling for testing purposes');
 };
 init();
